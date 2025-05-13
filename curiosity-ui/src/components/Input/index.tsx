eb5a07b18b771ca4758465098b5bfb2f627bb95f
barreiradlc ( Augusto Barreira ) <barreira266@hotmail.com>
2025-05-12 23:36:05 -0300

    Improvement in input

--- curiosity-ui/src/components/Input/index.tsx
+++ curiosity-ui/src/components/Input/index.tsx
@@ -5,7 +5,7 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 const Input: React.FC<InputProps> = (props) => {
   return (
     <input
-      className="border rounded-lg p-2 w-full"
+      className="border rounded-lg p-2 w-full focus:shadow"
       {...props}
     />
   );
